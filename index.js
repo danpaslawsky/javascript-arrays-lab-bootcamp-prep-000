a0e4d85fc9d62c2a7b421051fb0fa5ee3483c6cb
danpaslawsky <danpaslawsky@gmail.com>
2020-04-01 23:50:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
 
-// Add your functions and code here
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name);
+}
+
+function appendKitten(name){
+  kittens();
+  
+  
+}
